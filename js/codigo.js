4e05fe4b772e2d3c43ff09c8d94da4699728a55e
Maki Perdomi <macarenanatali@hotmail.com>
2022-09-06 21:14:46 -0300

    Agregando funciones

--- js/codigo.js
+++ js/codigo.js
@@ -1,31 +1,24 @@
 console.log("Productos");
 
-/* let remera = 1500
-let pantalon = 3000
-let pollera = 2000
-let buzo = 3500*/
+function comprar() {
+    let carrito = prompt("Agrega al carrito: remera, pollera, buzo o pantalon");
 
-let carrito = prompt("Agrega al carrito: remera, pollera, buzo o pantalon");
+    alert("Este es tu carrito: " + carrito);
 
-alert("Este es tu carrito: " + carrito);
+    if (carrito == "remera") {
+        alert("El precio es $1500 c/u")
+    } else if (carrito == "pollera") {
+        alert("El precio es $2000 c/u")
+    } else if (carrito == "pantalon") {
+        alert("El precio es $3000 c/u")
+    } else if (carrito == "buzo") {
+        alert("El precio es $3500 c/u")
+    } else {
+        alert("Sin stock")
+    };
+}
 
-if (carrito == "remera") {
-    alert("El precio es $1500 c/u")
-} else if (carrito == "pollera") {
-    alert("El precio es $2000 c/u")
-} else if (carrito == "pantalon") {
-    alert("El precio es $3000 c/u")
-} else if (carrito == "buzo") {
-    alert("El precio es $3500 c/u")
-} else {
-    alert("Sin stock")
-};
-
-/* let remera = {
-    color1: "Rojo",
-    color2: "Negro",
-    color3: "Blanco"
-};*/
+comprar()
 
 //Stock de remeras con "Do while"
 
@@ -49,4 +42,43 @@ while (stockRemeras > 0)
 for (let i = 15; i > 0; i--) {
 
     document.write(i + "<br>")
-    }
+}
+
+//Objetos: Productos
+
+const producto1 = {
+    nombre: "Remera",
+    precio: "1500",
+    color1: "Rojo",
+    color2: "Negro",
+    color3: "Blanco",
+}
+
+const producto2 = {
+    nombre: "Pantalon",
+    precio: "3000",
+    color1: "Azul",
+    color2: "Negro",
+}
+
+const producto3 = {
+    nombre: "Pollera",
+    precio: "2000",
+    color1: "Jean",
+    color2: "Negro",
+}
+
+const producto4 = {
+    nombre: "Buzo",
+    precio: "2000",
+    color1: "Rojo",
+    color2: "Violeta",
+    color3: "Amarillo",
+}
+
+const productos = [producto1, producto2, producto3, producto4];
+
+
+function agregarAlCarrito(params) {
+    
+}
